1d5a7f3b365269e57c43546f81c60b5c8702ba5c
Fong Zi Hao <PACE.K2320388H@assoc.main.ntu.edu.sg>
2023-05-02 21:07:02 +0800

    Assignment Done

--- assignment-code/MethodsAssignment.java
+++ assignment-code/MethodsAssignment.java
@@ -1,31 +1,58 @@
+import java.util.ArrayList;
+
 public class MethodsAssignment {
-    public static void main(String args[]){
-        //Uncomment these to test:
-        // checkOddEven(998);
-        // checkOddEven(1237);
+    public static void main(String args[]) {
+        // Uncomment these to test:
+        checkOddEven(998);
+        checkOddEven(1237);
 
-        //Uncomment these to test:
-        // System.out.println(capsCombine("john", "smith"));
-        // System.out.println(capsCombine("charlize","doe"));
+        // Uncomment these to test:
+        System.out.println(capsCombine("john", "smith"));
+        System.out.println(capsCombine("charlize", "doe"));
 
-        //Uncomment these to test:
-        // int test_arr1[] = {1, 2, 3, 4, 5};
-        // int test_arr2[] = {99, 100, 77, 62, 87, 92, 91, 100};
-        // System.out.println(getAverage(test_arr1));
-        // System.out.println(getAverage(test_arr2));
+        // Uncomment these to test:
+        int test_arr1[] = { 1, 2, 3, 4, 5 };
+        int test_arr2[] = { 99, 100, 77, 62, 87, 92, 91, 100 };
+        System.out.println(getAverage(test_arr1));
+        System.out.println(getAverage(test_arr2));
 
     }
 
-    //1. Create a method called checkOddEven that gets an integer and prints "The number is odd/even" depending on the result.
+    // 1. Create a method called checkOddEven that gets an integer and prints "The
+    // number is odd/even" depending on the result.
 
-    //Add code here
-    
-    //2. Create a method called capsCombine that gets two strings and converts both to uppercase and joins them with a space.
+    // Add code here
+    static void checkOddEven(int number) {
+        // code to be executed
+        String result = "The number is odd";
+        if (number % 2 == 0) {
+            result = "The number is even";
+        }
+        System.out.println(result);
+        return;
+    };
 
-    //Add code here
+    // 2. Create a method called capsCombine that gets two strings and converts both
+    // to uppercase and joins them with a space.
 
-    //3. Create a method called getAverage that gets an array of numbers and returns the average of the values in the array.
+    // Add code here
+    static String capsCombine(String stringOne, String stringTwo) {
+        // code to be executed
+        return (stringOne.toUpperCase() + " " + stringTwo.toUpperCase());
+    };
 
-    //Add code here
-        
+    // 3. Create a method called getAverage that gets an array of numbers and
+    // returns the average of the values in the array.
+
+    // Add code here
+    static float getAverage(int numbers[]) {
+        int total = 0;
+
+        for (int index = 0; index < numbers.length; index++) {
+            total += numbers[index];
+        }
+
+        return ((float) total / numbers.length);
+    }
+
 }
